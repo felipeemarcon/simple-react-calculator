2f5b9375f86ea2366c2865358e4fcfad9676e90e
Felipe Marcon <felipe.marcon@gamersclub.com.br>
2019-11-27 15:50:04 -0300

    init logic implementation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,6 +2,17 @@ import React from "react";
 
 import "./Button.css";
 
-const Button = props => <button className="button">{props.label}</button>;
+const Button = props => (
+  <button
+    onClick={() => props.click && props.click(props.label)}
+    className={`button
+    ${props.operation ? "operation" : ""}
+    ${props.double ? "double" : ""}
+    ${props.triple ? "triple" : ""}
+    `}
+  >
+    {props.label}
+  </button>
+);
 
 export default Button;
